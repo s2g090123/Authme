9c90614b9c97d6e15b0176e5ecd64613e341d911
杜佳謙 <allen.du@kdanmobile.com>
2024-03-16 17:29:44 +0800

    Update: Add InfoResponse

---
+++ GitHubUserSDK/src/main/java/com/example/githubusersdk/models/UserInfoResponse.kt
@@ -0,0 +1,19 @@
+package com.example.githubusersdk.models
+
+
+import com.google.gson.annotations.SerializedName
+
+data class UserInfoResponse(
+    @SerializedName("name")
+    val name: String?,
+    @SerializedName("bio")
+    val bio: String?,
+    @SerializedName("login")
+    val login: String?,
+    @SerializedName("site_admin")
+    val siteAdmin: Boolean?,
+    @SerializedName("location")
+    val location: String?,
+    @SerializedName("blog")
+    val blog: String?
+)
