a3502be0777bea6e71a1cd7c7e34b278daa46117
杜佳謙 <allen.du@kdanmobile.com>
2024-03-16 15:25:23 +0800

    New: Add necessary dependencies

--- GitHubUserSDK/build.gradle.kts
+++ GitHubUserSDK/build.gradle.kts
@@ -40,4 +40,11 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // testing
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
 }
